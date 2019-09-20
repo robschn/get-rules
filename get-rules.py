6ca7e7746826f92ac6af04190a86fafebc117f56
Robert Schneider <robmschn@gmail.com>
2019-09-20 14:55:29 -0400

    added disable SSL warning and gateway var

--- get-rules.py
+++ get-rules.py
@@ -5,14 +5,19 @@
 # imports
 import requests
 from xml.etree import ElementTree
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 # import username and password
 import creds
 keygen = creds.keygen
 
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
 r = requests.get('https://pan.rollins.edu//api/?type=op&cmd=<show><system><info></info></system></show>&key=' + keygen, verify=False)
 
 root = ElementTree.fromstring(r.content)
 
-print (root[0][0][1].text)
-print (root[0][0][1].tag)
+gateway = root[0][0][4]
+print (gateway.text)
+print (gateway.tag)
+print (gateway.attrib)
