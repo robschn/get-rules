4a3d49a5bdc33c1971db3f43496c993780b282a5
Robert Schneider <robmschn@gmail.com>
2019-09-20 15:27:22 -0400

    converted XML to JSON format using xmltodict

--- get-rules.py
+++ get-rules.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-# Requires Python3
+# Requires Python3 and xmltodict
 
 # imports
 import requests
-from xml.etree import ElementTree
+import json
+import xmltodict
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 # import username and password
@@ -14,10 +15,20 @@ keygen = creds.keygen
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 r = requests.get('https://pan.rollins.edu//api/?type=op&cmd=<show><system><info></info></system></show>&key=' + keygen, verify=False)
+content =  r.content
 
-root = ElementTree.fromstring(r.content)
+dict = xmltodict.parse(content, dict_constructor=dict)
 
-gateway = root[0][0][4]
-print (gateway.text)
-print (gateway.tag)
-print (gateway.attrib)
+system = dict['response']['result']['system']
+
+print (system['wildfire-release-date'])
+
+
+# XML with installing another library
+# from xml.etree import ElementTree
+# root = ElementTree.fromstring(r.content)
+#
+# gateway = root[0][0][4]
+# print (gateway.text)
+# print (gateway.tag)
+# print (gateway.attrib)
