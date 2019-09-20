cec6b1cddb938c12802849bd4289b679c610aaf9
Robert Schneider <robmschn@gmail.com>
2019-09-20 11:19:07 -0400

    added import request

--- get-rules.py
+++ get-rules.py
@@ -2,7 +2,13 @@
 
 # Requires Python3
 
+# imports
+import requests
+
 # import username and password
 import creds
 keygen = creds.keygen
 print (keygen)
+
+r = requests.get('https://github.com/timeline.json')
+print (r.json())
